bbc1875dcc6e2db792c806b5c365149b38a726e7
Uz <suriono@yahoo.com>
2017-09-07 08:39:31 -0500

    Add Competition project/folder

--- TeamCode/src/main/java/Competition/DriverControlled.java
+++ TeamCode/src/main/java/Competition/DriverControlled.java
@@ -1,27 +1,28 @@
-package TestCode;
+package Competition;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
 import Library.Chassis_motors;
 
-@TeleOp(name = "Driver mode with chassis class", group = "TestCode")
-public class TeleopV1_with_chassis_motors_class extends LinearOpMode
+@TeleOp(name = "Driver Controlled", group = "Competition")
+public class DriverControlled extends LinearOpMode
 {
-
     private Chassis_motors chassis_Object = null;  // declare chassis motors object
 
     public void runOpMode() throws InterruptedException
     {
         chassis_Object = new Chassis_motors(hardwareMap); // create chassis motors object
 
+
+
+
         waitForStart();
 
         while(opModeIsActive())
         {
 
-            // Use chassis method to run motor, without encoder
+            // Joystick chassis motor control
             chassis_Object.run_Motors_no_encoder(-gamepad1.left_stick_y, -gamepad1.right_stick_y);
 
             idle();
